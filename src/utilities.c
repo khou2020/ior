khou2020/ior-forked

modify output format

src/utilities.c
@@ -627,7 +627,7 @@ void ShowFileSystemSize(char *fileSystem) // this might be converted to an AIORI
         }
 
         if(outputFormat == OUTPUT_DEFAULT){
-          fprintf(out_resultfile, "%-20s: %s\n", "Path", realPath);
+          fprintf(out_resultfile, "#%%$: IOR_%-20s: %s\n", "Path", realPath);
           fprintf(out_resultfile, "%-20s: %.1f %s   Used FS: %2.1f%%   ",
                   "FS", totalFileSystemSizeHR, fileSystemUnitStr,
                   usedFileSystemPercentage);

khou2020/ior

src/utilities.c
@@ -655,6 +655,7 @@ void ShowFileSystemSize(char * filename, const struct ior_aiori * backend, void 
           fileSystemUnitStr = "TiB";
   }
   if(outputFormat == OUTPUT_DEFAULT){
+    fprintf(out_resultfile, "#%%$: IOR_%-20s: %s\n", "Path", filename);
     fprintf(out_resultfile, "%-20s: %s\n", "Path", filename);
     fprintf(out_resultfile, "%-20s: %.1f %s   Used FS: %2.1f%%   ",
             "FS", totalFileSystemSizeHR, fileSystemUnitStr,